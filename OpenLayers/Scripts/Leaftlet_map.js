85c144d0dcc1aaaeb000b30bd246c649fa96ba60
Hsuhui <la10120818@yahoo.com.tw>
2020-10-22 00:12:05 +0800

    2020/10/22

---
+++ OpenLayers/Scripts/Leaftlet_map.js
@@ -0,0 +1,24 @@
+﻿var map;
+(function ($) {
+    $(document).ready(function () {
+        $('#mapid').height(window.innerHeight);
+         map = L.map('mapid', {
+            center: [25.0677705, 121.5111187],
+            zoom: 10
+        });
+        L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            maxZoom: 14,
+            attribution: 'Map data: © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: © <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+        }).addTo(map);
+    });
+    
+   
+})($)
+
+function get_point() {
+    L.marker([25.033976, 121.5623502]).addTo(map)
+        .bindPopup('<h1>臺北101</h1>')
+        .openPopup();
+}
+
+
---
+++ OpenLayers/Scripts/map.js
@@ -0,0 +1,77 @@
+﻿
+var key =
+    'Your Mapbox access token from https://mapbox.com/ here';
+var vector = new ol.layer.Heatmap({
+    // 資料來源
+    source: new ol.source.Vector({
+        // 網址
+        url: 'https://openlayers.org/en/v4.6.5/examples/data/kml/2012_Earthquakes_Mag5.kml',
+        // 資料格式
+        format: new ol.format.KML({
+            extractStyles: false
+        })
+    }),
+    // 模糊度
+    blur: 10,
+    // 半徑
+    radius: 10
+});
+
+var map = new ol.Map({
+    controls: ol.control.defaults().extend([
+        new ol.control.FullScreen(),
+        new ol.control.MousePosition({
+            coordinateFormat: ol.coordinate.createStringXY(4),
+            projection: 'EPSG:4326',
+            className: 'custom-mouse-position',
+            target: document.getElementById('mouse-position')
+        }),
+        new ol.control.OverviewMap({}),
+        new ol.control.Rotate({
+            autoHide: false
+        }),
+        new ol.control.ScaleLine({}),
+        new ol.control.ZoomSlider({}),
+        new ol.control.ZoomToExtent({})
+    ]),
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM(),
+        }), vector],
+    target: 'map',
+    view: new ol.View({
+        center: ol.proj.fromLonLat([37.4057, 8.81566]),
+        zoom: 4,
+    }),
+});
+
+//function bindInputs(layerid, layer) {
+//    var visibilityInput = $(layerid + ' input.visible');
+//    visibilityInput.on('change', function () {
+//        layer.setVisible(this.checked);
+//    });
+//    visibilityInput.prop('checked', layer.getVisible());
+
+//    var opacityInput = $(layerid + ' input.opacity');
+//    opacityInput.on('input change', function () {
+//        layer.setOpacity(parseFloat(this.value));
+//    });
+//    opacityInput.val(String(layer.getOpacity()));
+//}
+//map.getLayers().forEach(function (layer, i) {
+//    bindInputs('#layer' + i, layer);
+//    if (layer instanceof ol.layer.Group) {
+//        layer.getLayers().forEach(function (sublayer, j) {
+//            bindInputs('#layer' + i + j, sublayer);
+//        });
+//    }
+//});
+
+//$('#layertree li > span')
+//    .click(function () {
+//        $(this).siblings('fieldset').toggle();
+//    })
+//    .siblings('fieldset')
+//    .hide();
+
+
